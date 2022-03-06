10f2186abf3a41e9e8da54256a55f3b86ccbb967
Programer-D <60812831+Programer-D@users.noreply.github.com>
2022-03-07 06:17:50 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# name: discourse-categories-suppressed
-# about: Suppress categories from latest topics page.
+# name: discourse-categories-suppressed-only-normal
+# about: Suppress categories from latest topics page only normal.
 # version: 0.1
 # url: https://github.com/vinothkannans/discourse-categories-suppressed
 
@@ -10,11 +10,10 @@ after_initialize do
   if TopicQuery.respond_to?(:results_filter_callbacks)
     remove_suppressed_category_topics = Proc.new do |list_type, result, user, options|
       category_ids = (SiteSetting.categories_suppressed_from_latest.presence || "").split("|").map(&:to_i)
-
       if category_ids.blank? || list_type != :latest || options[:category] || options[:tags]
         result
       else
-        result.where("topics.category_id NOT IN (#{category_ids.join(",")})")
+        result.where("topics.category_id NOT IN (#{category_ids.join(",")}) OR tu.notification_level IN (2,3)")
       end
     end
 
