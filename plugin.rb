3bd46b9e61668f7bb8f8cf61765159bab5d10c0a
Programer-D <60812831+Programer-D@users.noreply.github.com>
2022-03-07 17:36:38 +0900

    Don't go where when open topic

--- plugin.rb
+++ plugin.rb
@@ -10,10 +10,10 @@ after_initialize do
   if TopicQuery.respond_to?(:results_filter_callbacks)
     remove_suppressed_category_topics = Proc.new do |list_type, result, user, options|
       category_ids = (SiteSetting.categories_suppressed_from_latest.presence || "").split("|").map(&:to_i)
-      if category_ids.blank? || list_type != :latest || options[:category] || options[:tags]
+      if category_ids.blank? || list_type != :latest || options[:category] || options[:tags] || user.nil?
         result
       else
-begin
+        begin
           result.where("topics.category_id NOT IN (#{category_ids.join(",")}) OR tu.notification_level IN (2,3)")
         rescue => e
           result
